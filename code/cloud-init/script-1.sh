a4216f66ae8302ba32aa72d37d14204f00b91f19
Craig S. Cottingham <craig.cottingham@gmail.com>
2011-03-21 23:39:34 -0500

    Fixed yet another bash syntax error in example code

--- code/cloud-init/script-1.sh
+++ code/cloud-init/script-1.sh
@@ -2,12 +2,12 @@
 HOME=/home/ec2-user
 sleep 20
 echo 'running' > $HOME/script-1-output.txt
-if [ -f $HOME/script-2-output.txt ];
+if [ -f $HOME/script-2-output.txt ]; then
   echo 'script-2 has run' >> $HOME/script-1-output.txt
 else
   echo 'script-2 has not run' >> $HOME/script-1-output.txt
 fi
-if [ -f $HOME/script-3-output.txt ];
+if [ -f $HOME/script-3-output.txt ]; then
   echo 'script-3 has run' >> $HOME/script-1-output.txt
 else
   echo 'script-3 has not run' >> $HOME/script-1-output.txt
--- code/cloud-init/script-3.sh
+++ code/cloud-init/script-3.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 HOME=/home/ec2-user
 echo 'running' > $HOME/script-3-output.txt
-if [ -f $HOME/script-1-output.txt ];
+if [ -f $HOME/script-1-output.txt ]; then
   echo 'script-1 has run' >> $HOME/script-3-output.txt
 else
   echo 'script-1 has not run' >> $HOME/script-3-output.txt
 fi
-if [ -f $HOME/script-2-output.txt ];
+if [ -f $HOME/script-2-output.txt ]; then
   echo 'script-2 has run' >> $HOME/script-3-output.txt
 else
   echo 'script-2 has not run' >> $HOME/script-3-output.txt
