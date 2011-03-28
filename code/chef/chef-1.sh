d723cb05d84a01f229cbe5fa3e07feb6df305066
Craig S. Cottingham <craig.cottingham@gmail.com>
2011-03-27 19:17:55 -0500

    Still working through cookbooks and JSON configuration

--- code/chef/chef-1.sh
+++ code/chef/chef-1.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-HOME=/home/ec2-user
 
 # update system software
 yum -y upgrade
@@ -9,19 +8,12 @@ yum -y install gcc ruby ruby-devel ruby-libs rubygems
 gem update --system
 
 # install Chef
-gem install chef ohai --no-ri --no-rdoc
+gem install chef ohai
 
 # get Chef Solo configuration
 mkdir -p /etc/chef
-curl -O http://craigcottingham.github.com/code/chef/solo.tar.gz
-tar xvfz solo.tar.gz
-mv solo /etc/chef
-
-# get Chef cookbooks
-mkdir -p /var/chef
-curl -O http://craigcottingham.github.com/code/chef/cookbooks.tar.gz
-tar xvfz cookbooks.tar.gz
-mv cookbooks /var/chef
+curl -O http://craigcottingham.github.com/code/chef/solo.rb
+mv solo.rb /etc/chef
 
 # run Chef
-chef-solo -c /etc/chef/solo/solo.rb -j /etc/chef/solo/dna.json
+# chef-solo -c /etc/chef/solo.rb -j http://craigcottingham.github.com/code/chef/solo.json
