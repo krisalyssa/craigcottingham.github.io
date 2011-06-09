d91540e88aabca0c2d3f8f38a5f863f1097bb209
Craig S. Cottingham <craig.cottingham@gmail.com>
2011-06-09 18:53:12 -0500

    Update example files to match blog post
    Signed-off-by: Craig S. Cottingham <craig.cottingham@gmail.com>

--- code/chef/config.rb
+++ code/chef/config.rb
@@ -1,7 +1,6 @@
-# chef-solo -c /etc/chef/solo.rb -j http://craigcottingham.github.com/code/chef/solo.json
 file_cache_path  "/var/chef"
 cookbook_path    "/var/chef/cookbooks"
-recipe_url       "http://craigcottingham.github.com/code/chef/cookbooks.tar.gz"
+recipe_url       "https://s3.amazonaws.com/craigcottingham-blog/chef/cookbooks.tar.gz"
 log_level        :info
-log_location     STDOUT
-ssl_verify_mode  :verify_none
+log_location     "/var/log/chef.log"  # or STDOUT
+verbose_logging  true
--- code/chef/mynode.sh
+++ code/chef/mynode.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
+# start EC2 instance with
+# $ ec2run ami-8c1fece5 --instance-type t1.micro --group sg-XXXXXX --key $EC2_KEYPAIR_NAME \
+#          -f mynode.sh
+# followed by:
+# $ ec2addtag i-XXXXXXXX --tag Name=mynode
+
 # update system software
 yum -y upgrade
-yum -y install gcc ruby ruby-devel ruby-libs rubygems
+yum -y install gcc make ruby ruby-devel ruby-libs rubygems
 
 # update RubyGems
-gem update --system 1.6.2
+gem update --system
 
 # install Chef
-gem install chef ohai
+gem install chef ohai --no-rdoc --no-ri
+
+mkdir -p /etc/chef
 
 # get Chef Solo configuration
-mkdir -p /etc/chef
-curl -O http://craigcottingham.github.com/code/chef/solo.rb
-mv solo.rb /etc/chef
+curl -o /etc/chef/solo.rb https://s3.amazonaws.com/craigcottingham-blog/chef/config.rb
 
 # run Chef
-# chef-solo -c /etc/chef/solo.rb -j http://craigcottingham.github.com/code/chef/solo.json
+chef-solo -c /etc/chef/config.rb \
+          -j https://s3.amazonaws.com/craigcottingham-blog/chef/mynode.json
