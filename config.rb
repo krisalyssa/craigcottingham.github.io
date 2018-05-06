36be8e0f87dfd0e1bccbcb1d627f213afe3649f8
Craig S. Cottingham <craig@cottingham.net>
2018-05-05 19:22:12 -0500

    Configure build

--- config.rb
+++ config.rb
@@ -42,7 +42,11 @@ page "/*.txt", layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :gzip
+  activate :minify_css
+  # activate :minify_javascript
+
+  activate :asset_hash
+  # activate :asset_host, :host => '//YOURDOMAIN.cloudfront.net'
+end
