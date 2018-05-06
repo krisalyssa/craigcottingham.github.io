3466c0c6eb8bf74b0009441d53718660f42cedd8
Craig S. Cottingham <craig@cottingham.net>
2018-05-05 19:33:03 -0500

    Update config.rb for middleman-blog

--- config.rb
+++ config.rb
@@ -6,7 +6,28 @@ activate :autoprefixer do | prefix |
 end
 
 activate :blog do | blog |
-  # set options on blog
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
 end
 
 activate :livereload
@@ -18,6 +39,7 @@ activate :livereload
 page "/*.xml", layout: false
 page "/*.json", layout: false
 page "/*.txt", layout: false
+page "/feed.xml", layout: false
 
 # With alternative layout
 # page "/path/to/file.html", layout: "other_layout"
