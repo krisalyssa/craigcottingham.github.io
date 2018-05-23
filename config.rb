15deae920d99254159557f9f4e991a6df03df993
Craig S. Cottingham <craig@cottingham.net>
2018-05-23 09:41:06 -0500

    Turn on footnote support

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 set :markdown_engine, :redcarpet
-set :markdown, :fenced_code_blocks => true, :smartypants => true
+set :markdown, fenced_code_blocks: true, footnotes: true, smartypants: true
 
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
